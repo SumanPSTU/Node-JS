43a606804d112f7ac373801d1edecdc838d2f62b
sumanPSTU <ug2202014@cse.pstu.ac.bd>
2025-04-17 21:16:44 +0600

    find data

--- src/mongoDB/connection.js
+++ src/mongoDB/connection.js
@@ -4,23 +4,38 @@ import { MongoClient } from 'mongodb';
 const uri = "mongodb://127.0.0.1:27017";
 
 const client = new MongoClient(uri);
-async function connectoDatabase(){
-    try {
-        client.connect();
-        console.log('connected')
 
+async function connectToDatabase() {
+    try {
+        // Connect to the MongoDB server
+        await client.connect();
+        console.log('Connected to MongoDB');
+
+        // Access the 'school' database and 'student' collection
         const db = client.db('school');
         const collection = db.collection('student');
-        const document={
-            name:'sumon',
-            id:2202014,
-            faculty:'cse'
-        };
-        collection.insertOne(document);
-        console.log('data inserted')
 
-    }catch (err){
-        throw err;
+        // Insert a document into the 'student' collection
+        const document = {
+            name: 'Sujon',
+            id: 220014,
+            faculty: 'CSE'
+        };
+        await collection.insertOne(document);
+        console.log('Data inserted');
+
+        // Fetch all documents from the 'student' collection
+        const documents = await collection.find({}, {projection: {_id:0,name: 1, faculty: 1}}).toArray();
+        for (let i =0;i<documents.length;i++){
+            console.log("name is "+documents[i].name +" and faculty is "+documents[i].faculty)
+        }
+    } catch (err) {
+        console.error('Error:', err);
+    } finally {
+        await client.close(); // Close the connection
+        console.log('Connection closed');
     }
 }
-connectoDatabase();
+
+// Execute the function
+connectToDatabase();
