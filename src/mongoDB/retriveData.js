7c7faa32298de7f662fd0e829dee4b4a254acf43
sumanPSTU <ug2202014@cse.pstu.ac.bd>
2025-04-18 13:09:17 +0600

    add fileSystem.js

--- src/mongoDB/retriveData.js
+++ src/mongoDB/retriveData.js
@@ -1,1 +1,20 @@
-import
+import collection from "./connection.js";
+
+
+async function retriveData(){
+    try {
+       const coll  = await collection();
+       const document =await coll.find({$and:[
+               { name: 'sajol' },
+               { faculty: 'BBA' }
+           ]},{projection:{
+           name:1,_id:0,faculty:1
+           }}).toArray();
+       console.log(document)
+    }catch (err){
+        console.error(err)
+    }
+
+
+}
+retriveData();
